a2a2168a0712cc36e9fe45b4882d6d7f9a894eda
Anjanesh <anjaneshss@hotmail.com>
2019-03-28 13:05:36 +0530

    Added Employee Class

---
+++ MyVisualStudioProjects/HelloWorld/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Employee
+    {
+        public int EmpID { get; set; }
+        public String EmpName { get; set; }
+
+        public void GetEmpInfo()
+        {
+            Console.WriteLine("Employee ID : {0} and Name : {1}", EmpID, EmpName);
+        }
+    }
+}
