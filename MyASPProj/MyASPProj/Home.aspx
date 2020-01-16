6984c6ebc8dc929420c1e2171004e9b551d46885
Anjanesh <anjaneshss@hotmail.com>
2020-01-16 14:23:37 +0530

    Initial Commit

---
+++ MyASPProj/MyASPProj/Home.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="MyASPProj.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1 style="align-content:center">Welcome to ASP.NET</h1>
+
+        <asp:Label ID="Username" runat="server" Text="Label">Username</asp:Label>
+        &nbsp;&nbsp;<input id="Text1" type="text" id ="username"/>
+
+        <br /><asp:Label ID="Password" runat="server" Text="Label">Password</asp:Label>
+        &nbsp;&nbsp;<input type="password" id="password" />
+
+        <br /><br /><input type="submit" id="submit" value="SUBMIT" />
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ WCF/WelcomeWcfService/WelcomeWcfService/WelcomeWCFService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WelcomeWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WelcomeWCFService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select WelcomeWCFService.svc or WelcomeWCFService.svc.cs at the Solution Explorer and start debugging.
+    public class WelcomeWCFService : IWelcomeWCFService
+    {
+        public void DoWork()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Welcome(string name)
+        {
+            return "Welcome to my first WCF Application " + name;
+        }
+    }
+}
